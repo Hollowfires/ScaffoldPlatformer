3434d3c1e965b37e47fe44581539bc05969dbc5f
Jared <jaredclark11@gmail.com>
2019-11-14 13:48:37 -0800

    first implementation of gravity block
    blocks will fall and can be pushed dont know how to pull the yet because that would require us to have to grab them
    problem:does not collide with patroller enemies.

---
+++ Platformer/Assets/Scripts/GravityBlocks.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBlocks : MonoBehaviour
+{
+    void Start()
+    {
+        Physics2D.gravity = new Vector2(0, -9.8f);
+    }
+
+    void Update()
+    {
+    }
+}
